423cd571169341a780e3abe5c26ecc54b50f6948
Bui Van Yen <buivanyen215@gmail.com>
2025-10-05 01:23:52 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+
+#define ENCA 2
+#define ENCB 3
+#define PWM 5
+#define IN1 6
+#define IN2 7
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ENCA, INPUT);
+  pinMode(ENCB, INPUT);
+  pinMode(PWM, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+}
+
+void loop() {
+  int pwr=50;
+  int dir=1;
+  setMotor(dir, pwr, PWM, IN1, IN2);
+}
+
+void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
+  analogWrite(pwm, pwmVal);
+  if(dir == 1){
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+  } else if(dir == -1){
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+  } else {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+  }
+}
